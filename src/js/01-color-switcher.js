48d5550f4150219fc0a91f1bb8ed8f7a9b1c4d1c
Valeriia <volikavs@gmail.com>
2022-07-03 23:02:20 +0200

    up 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+
+btnStart.addEventListener('click', onClickBtnStart);
+btnStop.addEventListener('click', onClickBtnStop);
+
+
+function onClickBtnStart() {
+    console.log('onClickBtnStart');
+}
+
+function onClickBtnStop() {
+    console.log('onClickBtnStop');
+}
